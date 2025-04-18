f5c011f1a1bc320551c43e659e307ce8976dfe2c
marwhals <marjan.mubarok2012@gmail.com>
2025-04-19 00:15:17 +0100

    Setup project

---
+++ app/Main.hs
@@ -0,0 +1,11 @@
+module Main where
+
+import Cryptol.REPL.Monad
+import Cryptol.REPL.Command
+import Cryptol.Eval
+import Cryptol.ModuleSystem
+
+main :: IO ()
+main = do
+  putStrLn "Cryptol REPL embedding example coming soon..."
+  -- You can run commands, evaluate expressions, or interact with modules here
